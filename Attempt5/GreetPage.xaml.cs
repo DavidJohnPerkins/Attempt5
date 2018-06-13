63d5f740bed4f38ff8dcfd58bc477f7b24148d06
davidjperkins@outlook.com <davidjperkins@outlook.com>
2018-06-13 20:50:07 +0100

    Initial check-in of module Attempt5

---
+++ Attempt5/GreetPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Attempt5
+{
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+        }
+    }
+}
